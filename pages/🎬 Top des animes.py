42a3b490f8e55c432d54b7b92f9423a1734486f1
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-05-16 21:26:05 +0200

    Update 🎬 Top des animes.py

--- pages/🎬 Top des animes.py
+++ pages/🎬 Top des animes.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 from wordcloud import WordCloud
 from langdetect import detect
 from datetime import datetime
