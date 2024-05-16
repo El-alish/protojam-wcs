6a22a1bdb086a70dbe923b55a7f0c4c10643891f
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-05-16 22:30:17 +0200

    Update PowerBI.py

--- pages/PowerBI.py
+++ pages/PowerBI.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.html("https://app.powerbi.com/reportEmbed?reportId=216d0f56-6b9a-47b2-9d5a-f4b7498133db&autoAuth=true&ctid=017025da-c376-4d92-b318-b346be04e6cb", width=600, height=600)
+st.components.html("https://app.powerbi.com/reportEmbed?reportId=216d0f56-6b9a-47b2-9d5a-f4b7498133db&autoAuth=true&ctid=017025da-c376-4d92-b318-b346be04e6cb", width=600, height=600)
