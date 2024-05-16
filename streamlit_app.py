ae169da038597eaa0a942619034807a0cb1516c5
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-05-16 17:25:26 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
-st.write('hello')
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import streamlit.components.v1 as components
+
+st.header('ProtoJam Wild Code School AFL', divider='rainbow')
+
+with st.sidebar:
+    #st.image('https://media4.giphy.com/media/PqcBU8T4SeuO3kOMVu/giphy.gif?cid=ecf05e47ayiowxuub1416x1q3ttwa8hrcpfhbje9g2ivq1m1&ep=v1_gifs_related&rid=giphy.gif&ct=g')
+    st.image('https://cdn.discordapp.com/attachments/1210594061346672687/1240680856251531425/Designer_2.jpeg?ex=66477196&is=66462016&hm=e3ca395793ea5a222f3e435c5ad402ad2da2ac848b448384a441ccfe357b5947&')
+
+
+st.header('Analyse du monde de l\'animation japonaise en Mars 2024')
+st.write('Ce projet a été réalisé dans le cadre de la formation BootCamp Data Analyst, au sein de l\'école de formation Wild Code School, il a pour but de vous présenter une analyse du monde de l\'animation japonaise en Mars 2024. Vous pourrez y retrouver les sites les plus visités, les prochaines sorties et l\'équipe derrière le projet.')
+
+st.image('https://68.media.tumblr.com/58e04b9a1e9f5162ef2d1a0ace3c7221/tumblr_ojg1n3EPCG1w0ii2ho1_500.gif')
